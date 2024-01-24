b688568bd38193f564b64c1f04cebde8b37a2ff6
Sean Chang <zlac166@live.rhul.ac.uk>
2024-01-24 20:02:15 +0000

    Feature: Inheritance using Block Tags

--- Flask Templates/app.py
+++ Flask Templates/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template("index.html")
+  return render_template("index.html", template_recipes=recipes)
 
 @app.route("/recipe/<int:id>")
 def recipe(id):
